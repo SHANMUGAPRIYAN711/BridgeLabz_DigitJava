4abc36f616c2e43c0a22b6389b89e6e93acccf7d
SHANMUGAPRIYAN711 <shanmugapriyant711@gmail.com>
2026-04-21 10:55:39 +0530

    [SHANMUGAPRIYAN]: UC1 - Calculate line length

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,5 +1,10 @@
 public class LineComparison {
+    public static double CalculateLength(int x1, int y1, int x2, int y2){
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program");
+        double length = CalculateLength(0, 0, 3, 4);
+        System.out.println("Length of Line : " + length);
     }
 }
